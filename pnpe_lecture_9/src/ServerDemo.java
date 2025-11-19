46075288f4c20c3d395495b8d481557a0ccf2ea0
Marieta Stoycheva <MPStoycheva19@codingburgas.bg>
2025-11-19 12:26:01 +0200

    Add server and client implementation

---
+++ pnpe_lecture_9/src/ServerDemo.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerDemo {
+    public static void main(String[] args) throws IOException {
+        int number;
+        ServerSocket server = new ServerSocket(1211);
+        Socket socket = server.accept();
+        Scanner scan = new Scanner(socket.getInputStream());
+        number = scan.nextInt();
+        PrintStream printout = new PrintStream(socket.getOutputStream());
+        if ((number % 2) == 0) {
+            printout.println("your number is even");
+        } else {
+            printout.println("your number is odd");
+        }
+        server.close();
+        scan.close();
+    }
+}
